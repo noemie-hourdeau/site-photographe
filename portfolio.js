b82e2131e3cb4140163700b23146d61d828733d9
Bastien Daugenne <bastien.daugenne@gmail.com>
2021-07-30 09:17:26 +0200

    Dernier push avant merge final Bastien

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+// Affichage gallerie photos par catégories au clic sur "Afficher photos par catégories"
+
+
+// Affichage gallerie photos en totalité au clic sur "Afficher toutes les photos"
+
+
+// Affichage gallerie photos par sliders au clic sur input "-"
